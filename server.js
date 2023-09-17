0b9fadd7e9006fc5a2d7aedb402611b6942fd1f2
José Cardoso <zehcardoso@outlook.com>
2023-09-17 17:34:01 -0300

    Criando API REST com framework fastify

--- server.js
+++ server.js
@@ -1,9 +1,51 @@
-import { createServer } from 'node:http'
+import fastify from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
 
-const server = createServer((request, response) => {
-    response.write('Hello Word')
+const server = fastify()
 
-    return response.end()
+const database = new DatabaseMemory()
+
+server.post('/videos', (request, reply) => {
+  const { title, description, duration } = request.body
+
+  database.create({
+    title,
+    description,
+    duration
+  })
+
+  return reply.status(201).send()
 })
 
-server.listen(3333)
+server.get('/videos', (request) => {
+  const search = request.query.search
+
+  const videos = database.list(search)
+
+  return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+  const id = request.params.id
+  const { title, description, duration } = request.body
+
+  database.update(id, {
+    title,
+    description,
+    duration
+  })
+
+  return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+  const id = request.params.id
+
+  database.delete(id)
+
+  return reply.status(204).send()
+})
+
+server.listen({
+  port: 3333,
+})
